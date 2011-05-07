5f7dbec8d2c0cd378ab0cb87219ea4a58d10752d
dekpump <sakarin@webwarestudios.com>
2011-05-07 10:44:25 +0700

    Init Spree And Spree-Multi-Store

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = 'd982f0693c2def8a940acc681ac6e312a2b40fa70548d95e6602fa79568ed44244ea6377040fa5688faf4cd5650059f1876671e1f7a4030a20261d6c28a43b4d'
